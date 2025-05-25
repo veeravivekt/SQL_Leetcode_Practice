872b42500288930fec2048f155ab1f4667c1aee8
veeravivekt <veeravivek.telagani@sjsu.edu>
2025-05-25 08:29:32 -0700

    update Special bonus

--- 1873_Calculate_Special_Bonus.sql
+++ 1873_Calculate_Special_Bonus.sql
@@ -3,6 +3,11 @@ employee_id -> PK
 Table: employee ID, employee name, and salary
 Bonus is 100% if: ID = odd, name[0] != M
 */
-SELECT "employee_id", IF("employee_id" % 2 = 1 AND SUBSTRING("name", 1, 1) != 'M', "salary", 0) AS "bonus"
-FROM "Employees"
-ORDER BY "employee_id";
+SELECT employee_id, 
+CASE
+    WHEN employee_id % 2 <> 0
+    AND LEFT(name, 1) <> 'M' THEN salary
+    ELSE 0
+END AS bonus
+FROM Employees
+ORDER BY employee_id;
